0d0d21bec099ebf2779607fb935bceb0c796220f
Gokul Kumar Ragavan <goragav1@in.ibm.com>
2024-12-14 19:40:20 +0530

    Logout Page Rough Styles Added

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -16,10 +16,12 @@ const Logout = () => {
     }
 
     return (
-        <div>
-            <h3>Are you sure waant to Logout?</h3>
-            <button onClick={handleLogout}>Yes</button>
-            <button onClick={handleCancel}>No</button>
+        <div className="flex items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+            <h3>Are you sure want to Logout?</h3>
+            <button className="w-2/4 bg-blue-500 text-white py-2 px-4 rounded-md font-semibold hover:bg-blue-600 focus:outline-none" onClick={handleLogout}>Yes</button>
+            <button className="w-2/4 bg-white border border-solid border-blue-500 text-blue-500 py-2 px-4 rounded-md font-semibold hover:bg-blue-500 hover:text-white focus:outline-none" onClick={handleCancel}>No</button>
+        </div>
         </div>
     );
 }
